03e8eef4467889a9330d1f85f794f15feb9e40d3
adrianSavchuk <dev.work.adrian@gmail.com>
2024-09-14 06:18:59 +0300

    pull

--- src/Components/ MainMenu/MainMenu.tsx
+++ src/Components/ MainMenu/MainMenu.tsx
@@ -1,31 +1,45 @@
-import { Link } from "react-router-dom"
-import s from "./MainMenu.module.scss"
-import { useState } from "react"
-import logo from "../../assets/images/logo262x300.png"
+import { Link } from "react-router-dom";
+import s from "./MainMenu.module.scss";
+import { useState } from "react";
+import logo from "../../assets/images/logo262x300.png";
+import logoShadow from "../../assets/images/logo262x300shadow.png";
 
 const MainMenu = () => {
-    const [preview, setPreview] = useState(true)
-    const [logoAnimation, setLogoAnimation] = useState(false)
+  const [preview, setPreview] = useState(true);
+  const [logoAnimation, setLogoAnimation] = useState(false);
+  const [logoSrc, setLogoSrc] = useState(logo)
 
-
-        if (preview) {
-            return <img src={logo} className={`${s.logo} ${logoAnimation ? s.hidden : s.visible}`} onClick={() => {
-                setLogoAnimation(true)
-                setTimeout(() => setPreview(false), 500)
-            }} />
-        }
-
-
-    
-
+  if (preview) {
     return (
-        <div className={s.mainMenu}>
-            <Link className={s.mainMenuItem} to={"/projects"}>PROJECTS</Link>
-            <Link className={s.mainMenuItem} to={"/shop"}>SHOP</Link>
-            <Link className={s.mainMenuItem} to={"/contact"}>CONTACT</Link>
-            <Link className={s.mainMenuItem} to={"/about"}>ABOUT</Link>
-        </div>
-    )
-}
+      <img
+        src={logoSrc}
+        className={`${s.logo} ${logoAnimation ? s.hidden : s.visible}`}
+        onMouseEnter={() => setLogoSrc(logoShadow)}
+        onMouseLeave={() => setLogoSrc(logo)}
+        onClick={() => {
+          setLogoAnimation(true);
+          setTimeout(() => setPreview(false), 500);
+        }}
+      />
+    );
+  }
 
-export default MainMenu
+  return (
+    <div className={s.mainMenu}>
+      <Link className={s.mainMenuItem} to={"/projects"}>
+        PROJECTS
+      </Link>
+      <Link className={s.mainMenuItem} to={"/shop"}>
+        SHOP
+      </Link>
+      <Link className={s.mainMenuItem} to={"/contact"}>
+        CONTACT
+      </Link>
+      <Link className={s.mainMenuItem} to={"/about"}>
+        ABOUT
+      </Link>
+    </div>
+  );
+};
+
+export default MainMenu;
